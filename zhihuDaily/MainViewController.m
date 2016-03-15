88fc5a487027248e44b41c3a4b2dbe93e1bf87e3
SiegrainWong <siegrainwong@qq.com>
2016-03-15 21:42:09 +0800

    项目初始化

---
+++ zhihuDaily/MainViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zhihuDaily
+//
+//  Created by Siegrain on 16/3/15.
+//  Copyright © 2016年 siegrain.zhihuDaily. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
